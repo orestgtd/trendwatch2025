c570443d6f768badb5b5322fc3a20b50f069ba89
Orest Nahacziwec <orestgtd@gmail.com>
2025-08-08 22:55:45 -0400

    elm: chore cleanup Hello.elm by removing unused httpErrorToString function
    Removed commented-out httpErrorToString function to tidy up the code and improve readability.

--- devel/elm/src/Page/Hello.elm
+++ devel/elm/src/Page/Hello.elm
@@ -53,26 +53,6 @@ getHello =
         }
 
 
-{--
-httpErrorToString : Http.Error -> String
-httpErrorToString error =
-    case error of
-        Http.BadUrl url ->
-            "Bad URL: " ++ url
-
-        Http.Timeout ->
-            "Request timed out"
-
-        Http.NetworkError ->
-            "Network error"
-
-        Http.BadStatus statusCode ->
-            "Bad status: " ++ String.fromInt statusCode
-
-        Http.BadBody msg ->
-            "Bad body: " ++ msg
---}
-
 -- VIEW
 
 view : Model -> Html Msg
