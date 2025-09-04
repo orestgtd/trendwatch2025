5a2d0fdbfb585222f4e9bc1b8d2be3478b466367
Orest Nahacziwec <orestgtd@gmail.com>
2025-09-04 12:54:29 -0400

    laravel: chore: move User model to Infrastructure/Laravel/Eloquent/User namespace
    Move the standard Laravel User model into the Infrastructure layer to maintain a clear separation of architectural layers.

--- devel/laravel/app/Infrastructure/Laravel/Eloquent/User/Models/User.php
+++ devel/laravel/app/Infrastructure/Laravel/Eloquent/User/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Infrastructure\Laravel\Eloquent\User\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
