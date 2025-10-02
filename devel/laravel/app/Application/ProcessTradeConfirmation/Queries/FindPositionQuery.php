33aab790d69f45f114a2cc551cd4fdc39b4f5439
Orest Nahacziwec <orestgtd@gmail.com>
2025-10-02 18:34:52 -0400

    laravel: refactor: standardize naming convention for Queries

---
+++ devel/laravel/app/Application/ProcessTradeConfirmation/Queries/FindPositionQuery.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Application\ProcessTradeConfirmation\Queries;
+
+use App\Domain\Position\{
+    Model\Position,
+};
+
+use App\Domain\Security\{
+    ValueObjects\SecurityNumber,
+};
+
+use App\Infrastructure\Laravel\Eloquent\Position\{
+    Dto\PersistedPositionDto,
+    Repositories\EloquentPositionRepository as PositionRepository,
+};
+
+use App\Domain\Position\Builders\BuildPositionFromPersisted;
+
+final class FindPositionQuery
+{
+    public function __construct(
+        private readonly PositionRepository $repository
+        ){}
+
+    public function findBySecurityNumber(SecurityNumber $securityNumber): ?Position
+    {
+        $persisted = $this->repository->findBySecurityNumber($securityNumber);
+
+        return $persisted
+        ? $this->buildPositionFromPersisted($persisted)
+        : null;
+    }
+
+    private function buildPositionFromPersisted(PersistedPositionDto $persisted): Position
+    {
+        return BuildPositionFromPersisted::from(
+            $persisted->securityNumber,
+            $persisted->positionType,
+            $persisted->positionQuantity,
+            $persisted->totalCost,
+            $persisted->totalProceeds
+        );
+    }
+}
