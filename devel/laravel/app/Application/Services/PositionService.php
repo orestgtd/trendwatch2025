93da753262d6130229d35f007993427f6cf4f02b
Orest Nahacziwec <orestgtd@gmail.com>
2025-10-02 11:45:07 -0400

    safety commit: PositionService (dirty code)

--- devel/laravel/app/Application/Services/PositionService.php
+++ devel/laravel/app/Application/Services/PositionService.php
@@ -6,7 +6,8 @@ use App\Domain\{
     Confirmation\Outcome\ConfirmationOutcome,
     Position\Outcome\PositionOutcomePlaceholder,
 };
-
+use App\Domain\Confirmation\Model\Confirmation;
+use App\Domain\Security\ValueObjects\SecurityNumber;
 use App\Shared\Result;
 
 final class PositionService
@@ -21,8 +22,24 @@ final class PositionService
      */
     public function updatePosition(ConfirmationOutcome $confirmationOutcome): Result
     {
+        $x = $this->processConfirmation($confirmationOutcome->getConfirmation());
+
+        dd($x);
+
         return Result::success(
             new PositionOutcomePlaceholder()
         );
+    }
+
+    private function processConfirmation(Confirmation $confirmation): void
+    {
+        $lookup = $this->lookupPosition($confirmation->getSecurityNumber());
+
+        dd($lookup);
+    }
+
+    private function lookupPosition(SecurityNumber $securityNumber): void
+    {
+        dd($securityNumber);
     }
 }
